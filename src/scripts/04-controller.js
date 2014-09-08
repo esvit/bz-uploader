dc1b228462d8bcd8d203ee5b9638cceeac1c1dce
Vitalii Savchuk <esvit666@gmail.com>
2014-09-08 13:02:20 +0300

    update file uploader version

--- src/scripts/04-controller.js
+++ src/scripts/04-controller.js
@@ -22,7 +22,7 @@ var bzUploaderController = ['$scope', 'FileUploader', '$parse', function($scope,
     };
 
     uploader.onSuccessItem = function (item, response, status, headers) {
-        response = $parse(response)();
+        response = angular.fromJson(response);
         if($scope.limit == 1) {
             $scope.files = $scope.files || '';
             $scope.files = response;
